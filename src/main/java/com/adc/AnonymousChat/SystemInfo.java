8815369bbf218ca684f95ac580e103a82df20f42
zioposty <l.postiglione4@studenti.unisa.it>
2020-10-14 14:05:32 +0200

    First commit

---
+++ src/main/java/com/adc/AnonymousChat/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.adc.AnonymousChat;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.adc.AnonymousChat {
+    requires javafx.controls;
+    exports com.adc.AnonymousChat;
+}

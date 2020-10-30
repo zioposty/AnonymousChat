ef7e6c3630c7f14e82417c74d6b05c377b9cccea
zioposty <l.postiglione4@studenti.unisa.it>
2020-10-30 17:29:08 +0100

    "Room creation with and without join it" implemented

--- src/main/java/it/isislab/p2p/anonymouschat/ChatViewController.java
+++ src/main/java/it/isislab/p2p/anonymouschat/ChatViewController.java
@@ -48,16 +48,83 @@ public class ChatViewController {
         if (result.isPresent()) {
             String roomName = result.get();
             //if(manager.getPeer().joinRoom(roomName))
-            if (true) {
+            if (true)
+                addTabChat(roomName);
+            else {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Error");
+                alert.setHeaderText("Operation failed");
+                alert.setContentText("Something went wrong, maybe the room doesn't exist");
 
-                TextArea chat = new TextArea();
-                chat.setId(roomName);
-                manager.addChat(roomName, chat);
-                chat.setPrefHeight(CHAT_SIZE[0]);
-                chat.setPrefWidth(CHAT_SIZE[1]);
-                Tab tab = new Tab(roomName, chat);
+                alert.showAndWait();
+            }
+        }
+    }
 
-                tab.setOnCloseRequest(e -> removeChat(e));
+
+    public boolean confirmJoinRoom(String name){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Join room");
+        alert.setHeaderText("Do you wonna join the created room: " + name);
+        alert.setContentText("Choose your option");
+
+        ButtonType yes = new ButtonType("Yes");
+        ButtonType no = new ButtonType("No");
+
+        alert.getButtonTypes().setAll(yes, no);
+        Optional<ButtonType> result = alert.showAndWait();
+
+        return result.get() == yes;
+
+    }
+
+    public void createRoom(ActionEvent actionEvent) {
+        TextInputDialog dialog = new TextInputDialog("roomName");
+        dialog.setTitle("Create room");
+        dialog.setHeaderText("Insert the name for the room that you wonna create (it must be unique)");
+        dialog.setContentText("Write here the room name:");
+
+        Optional<String> result =  dialog.showAndWait();
+        if(result.isPresent()) {
+            String roomName = result.get();
+                //if(manager.getPeer().createRoom(roomName))
+                if(false)
+                    if(confirmJoinRoom(roomName)){
+                        addTabChat(roomName);
+                    }
+                    else{
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Room created");
+                        alert.setHeaderText(null);
+                        alert.setContentText("The room was created. You can join it later");
+
+                        alert.showAndWait();
+                    }
+                else
+                {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Operation failed");
+                    alert.setContentText("Something went wrong, maybe the name isn't unique");
+                    alert.showAndWait();
+                }
+
+
+        }
+
+    }
+
+    private void addTabChat(String roomName) {
+        TextArea chat = new TextArea();
+        chat.setId(roomName);
+        manager.addChat(roomName, chat);
+        chat.setPrefHeight(CHAT_SIZE[0]);
+        chat.setPrefWidth(CHAT_SIZE[1]);
+        Tab tab = new Tab(roomName, chat);
+
+        tab.setOnCloseRequest(e -> removeChat(e));
+        chatTabs.getTabs().add(tab);
+
 
                 /*
                 Version with no Lamba expression
@@ -77,15 +144,6 @@ public class ChatViewController {
                 });
                 */
 
-                chatTabs.getTabs().add(tab);
-            } else {
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.setTitle("Error");
-                alert.setHeaderText("Operation failed");
-                alert.setContentText("Something went wrong, maybe the room doesn't exist");
 
-                alert.showAndWait();
-            }
-        }
     }
 }
